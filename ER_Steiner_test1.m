c31d7d547b65d085846c43846f376ba958b5d1a0
Amy Vogel <amy.lauren.vogel@gmail.com>
2020-04-20 23:36:14 -0400

    added animation for getting smallest steiner tree for each node

--- ER_Steiner_test1.m
+++ ER_Steiner_test1.m
@@ -133,127 +133,146 @@ for p=1:foodnum
 
     %%% iteration of D and P
     for k=1:kk  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% 
-    NUM=zeros(N,1);
-    %%%%%%%%%%%%%% whether a vertex exist
-    linknum=zeros(N,1);  %% how many tubes linked to a certain vertex
-    for i=1:N
-        for j=1:N
-            if set(i,j)==1
-                linknum(i)=linknum(i)+1;
+        NUM=zeros(N,1);
+        %%%%%%%%%%%%%% whether a vertex exist
+        linknum=zeros(N,1);  %% how many tubes linked to a certain vertex
+        for i=1:N
+            for j=1:N
+                if set(i,j)==1
+                    linknum(i)=linknum(i)+1;
+                end
+            end
+            if linknum(i)==0
+                exist(i)=0;
             end
         end
-        if linknum(i)==0
-            exist(i)=0;
+        vertex=sum(exist);  %%% number of existing vertex
+
+        %%%%%%%%%%%%%%%%%%%%%%%%%%% unequal possibility 3
+        adjacentlength=zeros(foodnum,1);
+        for i=1:foodnum
+             for j=1:N
+                  if set(food(i),j)==1
+                      adjacentlength(i)=adjacentlength(i)+L(food(i),j);
+                  end
+             end
         end
-    end
-    vertex=sum(exist);  %%% number of existing vertex
+        [B,ind]=sort(adjacentlength);   %%%  ascending order,  B is the ordered vector, ind is the intex
+        TotalAdjacent=0;
+        for i=1:foodnum
+            TotalAdjacent=TotalAdjacent+B(i);
+        end
 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%% unequal possibility 3
-    adjacentlength=zeros(foodnum,1);
-    for i=1:foodnum
-         for j=1:N
-              if set(food(i),j)==1
-                  adjacentlength(i)=adjacentlength(i)+L(food(i),j);
-              end
-         end
-    end
-    [B,ind]=sort(adjacentlength);   %%%  ascending order,  B is the ordered vector, ind is the intex
-    TotalAdjacent=0;
-    for i=1:foodnum
-        TotalAdjacent=TotalAdjacent+B(i);
-    end
+        sink = food(p);
+        luck = p;
 
-    sink = food(p);
-    luck = p;
-
-    %%%%%%%%%%%%%%   calculate the pressure
-    A=zeros(vertex-1);  
-    g=0; 
-    for w1=1:N   
-        if exist(w1)==1   %%% only calculate the existing vertice
-            if w1==sink %%% neglect the vertex if it's the sink point
-                ;
-            else
-                g=g+1; %% row number of A
-                h=0; 
-                NUM(w1)=g; %%%  record the row number's relationship with vertex number, which will be used when using pressure
-                for w3=1:N  %%%%%%%  the vertex corresponds to the columes of A
-                    if exist(w3)==1  %%% only calculate the existing point
-                         if w3==sink(1)  %%% neglect the vertex if it's the sink point
-                             ;
-                         else
-                                h=h+1; %% colume number of A
-                                if w3==w1   %%%  the row vertex is the colume vertex
-                                    for i=1:N
-                                        if set(w1,i)==1
-                                            A(g,h)=A(g,h)-D(w1,i)/L(w1,i);
+        %%%%%%%%%%%%%%   calculate the pressure
+        A=zeros(vertex-1);  
+        g=0; 
+        for w1=1:N   
+            if exist(w1)==1   %%% only calculate the existing vertice
+                if w1==sink %%% neglect the vertex if it's the sink point
+                    ;
+                else
+                    g=g+1; %% row number of A
+                    h=0; 
+                    NUM(w1)=g; %%%  record the row number's relationship with vertex number, which will be used when using pressure
+                    for w3=1:N  %%%%%%%  the vertex corresponds to the columes of A
+                        if exist(w3)==1  %%% only calculate the existing point
+                             if w3==sink(1)  %%% neglect the vertex if it's the sink point
+                                 ;
+                             else
+                                    h=h+1; %% colume number of A
+                                    if w3==w1   %%%  the row vertex is the colume vertex
+                                        for i=1:N
+                                            if set(w1,i)==1
+                                                A(g,h)=A(g,h)-D(w1,i)/L(w1,i);
+                                            end
+                                        end
+                                    else    %%%%   the row vertex is not the colume vertex
+                                        if set(w1,w3)==1
+                                            A(g,h)=D(w1,w3)/L(w1,w3);
                                         end
                                     end
-                                else    %%%%   the row vertex is not the colume vertex
-                                    if set(w1,w3)==1
-                                        A(g,h)=D(w1,w3)/L(w1,w3);
-                                    end
-                                end
-                         end
+                             end
+                        end
                     end
                 end
             end
         end
-    end
 
-    X=zeros(vertex-1,1); 
-    %%%%%%%%%%%%% all foodpoints that are not sink are sources
-    for i=1:foodnum
-        if i==luck
-            ;
+        X=zeros(vertex-1,1); 
+        %%%%%%%%%%%%% all foodpoints that are not sink are sources
+        for i=1:foodnum
+            if i==luck
+                ;
+            else
+                X(NUM(food(i)))=-I;
+            end
+        end
+
+        P=A\X;
+
+        %%%%calculate the flux and update the conductivity
+        for i=1:N
+            for j=1:N
+            if set(i,j)==1 
+                if i==sink
+                    Q(i,j)=D(i,j)/L(i,j)*(0-P(NUM(j)));
+                        D(i,j)=edgevalue(i,j)*(D(i,j)+alpha*abs(Q(i,j))-sigma*D(i,j));
+                        if D(i,j)<cutoff  
+                          set(i,j)=0; set(j,i)=0;
+                        end
+                end
+                if j==sink
+                    Q(i,j)=D(i,j)/L(i,j)*(P(NUM(i))-0);
+                        D(i,j)=edgevalue(i,j)*(D(i,j)+alpha*abs(Q(i,j))-sigma*D(i,j));
+                        if D(i,j)<cutoff  
+                          set(i,j)=0; set(j,i)=0;
+                        end
+                end
+                if i~=sink && j~=sink
+                    Q(i,j)=D(i,j)/L(i,j)*(P(NUM(i))-P(NUM(j)));
+                        D(i,j)=edgevalue(i,j)*(D(i,j)+alpha*abs(Q(i,j))-sigma*D(i,j));
+                        if D(i,j)<cutoff  
+                          set(i,j)=0; set(j,i)=0;
+                        end
+                end
+            end
+            end
+        end
+        fe_PO=fe_PO+1; % function evaluation
+        Total_length=0;  %%%  total length of the network
+        for i=1:N
+            for j=i:N
+                if set(i,j)==1
+                    Total_length=Total_length+L(i,j);
+                end
+            end
+        end
+        Total_length=Total_length+fuseL;
+        fit_PO(fe_PO)=Total_length;
+
+        EDGE(k)=sum(sum(set))/2;  %%%  record the evolution
+        
+        %%%% PLOTTING STEINER TREE SO FAR, ANIMATION %%%%%%
+        % this is showing how the best steiner tree is found for each node
+        figure(3);
+        if k == 1
+            baseSet = set;
+            pl1 = plot(graph(baseSet));
+        elseif k==kk
+            pl1 = plot(graph(baseSet));
+            highlight(pl1, graph(set));
+            highlight(pl1, [p], 'NodeColor', 'r', 'EdgeColor', 'r', 'LineWidth', 3);
+            pause(1);
         else
-            X(NUM(food(i)))=-I;
+            pl1 = plot(graph(baseSet));
+            highlight(pl1, graph(set));
+            highlight(pl1, [p], 'NodeColor', 'r', 'EdgeColor', 'r');
+            pause(.1);
         end
-    end
-
-    P=A\X;
-
-    %%%%calculate the flux and update the conductivity
-    for i=1:N
-        for j=1:N
-        if set(i,j)==1 
-            if i==sink
-                Q(i,j)=D(i,j)/L(i,j)*(0-P(NUM(j)));
-                    D(i,j)=edgevalue(i,j)*(D(i,j)+alpha*abs(Q(i,j))-sigma*D(i,j));
-                    if D(i,j)<cutoff  
-                      set(i,j)=0; set(j,i)=0;
-                    end
-            end
-            if j==sink
-                Q(i,j)=D(i,j)/L(i,j)*(P(NUM(i))-0);
-                    D(i,j)=edgevalue(i,j)*(D(i,j)+alpha*abs(Q(i,j))-sigma*D(i,j));
-                    if D(i,j)<cutoff  
-                      set(i,j)=0; set(j,i)=0;
-                    end
-            end
-            if i~=sink && j~=sink
-                Q(i,j)=D(i,j)/L(i,j)*(P(NUM(i))-P(NUM(j)));
-                    D(i,j)=edgevalue(i,j)*(D(i,j)+alpha*abs(Q(i,j))-sigma*D(i,j));
-                    if D(i,j)<cutoff  
-                      set(i,j)=0; set(j,i)=0;
-                    end
-            end
-        end
-        end
-    end
-    fe_PO=fe_PO+1; % function evaluation
-    Total_length=0;  %%%  total length of the network
-    for i=1:N
-        for j=i:N
-            if set(i,j)==1
-                Total_length=Total_length+L(i,j);
-            end
-        end
-    end
-    Total_length=Total_length+fuseL;
-    fit_PO(fe_PO)=Total_length;
-
-    EDGE(k)=sum(sum(set))/2;  %%%  record the evolution
+         
     end  %%%% k iteration
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     min(fit_PO)
